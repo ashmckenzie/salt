566ba2e675b7399dacf5301016879c6f94123ea9
Ash McKenzie <ash@the-rebellion.net>
2015-04-11 17:33:12 +1000

    More woops

--- modules/runners/custom.py
+++ modules/runners/custom.py
@@ -20,17 +20,29 @@ def pkg_list_upgrades():
     raw_output = client.cmd('*', 'pkg.list_upgrades')
     output = yaml.dump(raw_output, default_flow_style=False)
 
-    cfg = __opts__
-    from_addr = cfg.get('smtp.from', None)
-    to_addrs = cfg.get('smtp.to', None)
-    host = cfg.get('smtp.host', None)
-    port = cfg.get('smtp.port', 25)
-    user = cfg.get('smtp.username', None)
-    passwd = cfg.get('smtp.password', None)
-    subject = cfg.get('smtp.subject', None)
-    gpgowner = cfg.get('smtp.gpgowner', None)
-    fields = cfg.get('smtp.fields', '').split(',')
-    smtp_tls = cfg.get('smtp.tls', False)
+    if 'config.option' in __salt__:
+        from_addr = __salt__['config.option']('smtp.from')
+        to_addrs = __salt__['config.option']('smtp.to')
+        host = __salt__['config.option']('smtp.host')
+        port = __salt__['config.option']('smtp.port')
+        user = __salt__['config.option']('smtp.username')
+        passwd = __salt__['config.option']('smtp.password')
+        subject = __salt__['config.option']('smtp.subject')
+        gpgowner = __salt__['config.option']('smtp.gpgowner')
+        fields = __salt__['config.option']('smtp.fields').split(',')
+        smtp_tls = __salt__['config.option']('smtp.tls')
+    else:
+        cfg = __opts__
+        from_addr = cfg.get('smtp.from', None)
+        to_addrs = cfg.get('smtp.to', None)
+        host = cfg.get('smtp.host', None)
+        port = cfg.get('smtp.port', 25)
+        user = cfg.get('smtp.username', None)
+        passwd = cfg.get('smtp.password', None)
+        subject = cfg.get('smtp.subject', '')
+        gpgowner = cfg.get('smtp.gpgowner', None)
+        fields = cfg.get('smtp.fields', '').split(',')
+        smtp_tls = cfg.get('smtp.tls', False)
 
     subject += ' Available package upgrades'
 
