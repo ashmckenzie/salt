4ccd3c7fdcca0df6a363810c2e33a54f0c9c627b
Ash McKenzie <ash@the-rebellion.net>
2015-04-16 22:36:57 +1000

    Minor formatting fixes

--- reactor/sync_grains.sls
+++ reactor/sync_grains.sls
@@ -1,4 +1,4 @@
 # http://docs.saltstack.com/en/latest/topics/reactor/index.html#minion-start-reactor
 sync_grains:
   local.saltutil.sync_grains:
-    - tgt: {{ data['id'] }}
+    - tgt: {{data['id']}}
--- states/base/_grains/reboot_required.py
+++ states/base/_grains/reboot_required.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os.path
 
 def reboot_required():
   grains = {}
   grains['reboot_required'] = os.path.isfile('/var/run/reboot-required')
+
   return grains
